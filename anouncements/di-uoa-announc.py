91beecd0c2c88817f6676ed9988fc3bfa7c004ad
themisAnagno <thmanagnostopoulos@gmail.com>
2020-04-23 13:21:47 +0000

    Scrapper for di.uoa announcements

---
+++ anouncements/di-uoa-announc.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+import requests
+
+# Base url
+host = "http://www.di.uoa.gr"
+url = host
+
+# Get the HTML page
+html = requests.get(url).content
+
+# Create the soup from the html page
+soup = BeautifulSoup(html)
+
+# Get the announcement panel
+news_panel = soup.find("div", class_="panel-pane pane-views pane-news")
+
+# Get the news container
+news_container = news_panel.find("div", class_="view-content")
+
+news_list = news_container.find_all("div", class_="views-row")
+
+for item in news_list:
+    date = item.find("div", class_="views-field-created").span.text
+    title = item.find("div", class_="views-field-title").a.text
+    link = host + item.find("div", class_="views-field-title").a["href"]
+
+    output = {"date": date, "title": title, "link": link}
+    print(output)
